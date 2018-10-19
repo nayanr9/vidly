78603c4a671a160c19baf5e299c4df2c79442fe5
Raj Nayan <Ihcmhslciy~2008>
2018-10-20 02:07:28 +0530

    allowed 3001 port

--- index.js
+++ index.js
@@ -15,7 +15,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use(helmet());
-app.use(cors());
+app.use(cors(
+    origin = 'http://localhost:3001'
+));
 if (app.get('env') === 'development'){
     app.use(morgan('tiny'));
     console.log('Morgan Enabled...');
